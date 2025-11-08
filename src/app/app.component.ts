03ae461c4be8d48fc55ba4adde5fe772e3a1235f
mero93m <merabkopaleishvili1993@gmail.com>
2025-11-08 12:01:53 +0400

    feat: add routing, add http client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+import { NavigationComponent } from "./navigation/navigation.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, NavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { UsersTableComponent } from './users-table/users-table.component';
+import { PostsTableComponent } from './posts-table/posts-table.component';
+import { PromotionsComponent } from './promotions/promotions.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'users', component: UsersTableComponent, title: 'მომხმარებლები' },
+  { path: 'posts', component: PostsTableComponent, title: 'პოსტები' },
+  { path: 'promotions', component: PromotionsComponent, title: 'აქციები' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home' },
+];
