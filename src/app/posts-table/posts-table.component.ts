9413a77401664f957ef0c0ba1c32ff7248398749
mero93m <merabkopaleishvili1993@gmail.com>
2025-11-08 11:35:11 +0400

    init: init app

---
+++ src/app/posts-table/posts-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-posts-table',
+  imports: [],
+  templateUrl: './posts-table.component.html',
+  styleUrl: './posts-table.component.css'
+})
+export class PostsTableComponent {
+
+}
