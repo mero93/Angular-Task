db4575fd801f6b5a646c9527ed8554d3e7a0f8c1
mero93m <merabkopaleishvili1993@gmail.com>
2025-11-09 04:54:31 +0400

    feat: add navigation

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'app-navigation',
-  imports: [],
+  imports: [ RouterLink, RouterLinkActive],
   templateUrl: './navigation.component.html',
-  styleUrl: './navigation.component.css'
+  styleUrl: './navigation.component.css',
 })
-export class NavigationComponent {
-
-}
+export class NavigationComponent {}
