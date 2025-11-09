70d1d438cb431bca8a235a3409179b76ba503341
mero93m <merabkopaleishvili1993@gmail.com>
2025-11-09 05:43:13 +0400

    feat: add navigation, improve responsiveness for header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,29 +1,40 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
 import { map, Subscription, timer } from 'rxjs';
 import { DatePipe } from '@angular/common';
-import { AnimateClickDirective } from "../directives/animate-click.directive";
+import { AnimateClickDirective } from '../directives/animate-click.directive';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [DatePipe, AnimateClickDirective],
+  imports: [DatePipe, AnimateClickDirective, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
 export class HeaderComponent implements OnInit, OnDestroy {
   time = new Date();
-  subscription: Subscription | undefined;
+  timerSubscription: Subscription | undefined;
+  dateFormat = '';
 
   ngOnInit(): void {
-    this.subscription = timer(0, 1000)
+    this.timerSubscription = timer(0, 1000)
       .pipe(map(() => new Date()))
       .subscribe((time) => {
         this.time = time;
       });
+    this.onWindowResize();
   }
 
   ngOnDestroy(): void {
-    if (this.subscription) {
-      this.subscription.unsubscribe();
+    if (this.timerSubscription) {
+      this.timerSubscription.unsubscribe();
     }
   }
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize = () => {
+    this.dateFormat =
+      window.innerWidth < 768
+        ? 'dd/MM/yyyy, h:mm:ss a'
+        : 'dd MMM, yyyy, h:mm:ss a';
+  };
 }
